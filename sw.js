300ca79c658bfe96fc49a54d6090feacfba18dc8
Befalamp <74116863+Befalamp@users.noreply.github.com>
2026-09-22 13:45:43 +0100

    v4 - permit save

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nights-away-log-v3';
+const CACHE = 'nights-away-log-v4';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
