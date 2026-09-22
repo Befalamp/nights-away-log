15cddc20d61175fbb222396dad8878bccae13ae5
Befalamp <74116863+Befalamp@users.noreply.github.com>
2026-09-22 12:27:29 +0100

    v2 add section

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nights-away-log-v1';
+const CACHE = 'nights-away-log-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
