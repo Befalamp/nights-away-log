fad26c76e34b29eec8133fbf705250f27f0ec553
Befalamp <74116863+Befalamp@users.noreply.github.com>
2026-09-22 13:35:45 +0100

    v3 - add in edit

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nights-away-log-v2';
+const CACHE = 'nights-away-log-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
